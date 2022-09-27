9ef41409e63b8072d1d668697d4d91f11fd4228f
Aaditya Chandrasekhar <aadityac@gmail.com>
2022-09-27 16:54:55 +0000

    Set minimum macOS version to v12.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "vapor",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v12),
         .iOS(.v13),
         .tvOS(.v13),
         .watchOS(.v6)
